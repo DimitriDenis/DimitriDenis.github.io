66a8ab7485453c7d98a2f1cfd8af6c0ac64d360a
DimitriDenis <47151006+DimitriDenis@users.noreply.github.com>
2022-03-06 18:35:44 +0100

    second commit

---
+++ src/app/list-champion/list-champion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-champion',
+  templateUrl: './list-champion.component.html',
+  styleUrls: ['./list-champion.component.css']
+})
+export class ListChampionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
